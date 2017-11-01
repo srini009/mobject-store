106bd3b10b328f43c2b6064f5fdcd92eabe3cf6f
Shane Snyder <ssnyder@mcs.anl.gov>
2017-11-01 12:22:22 -0500

    test utilities for mobject

---
+++ tests/mobject-test-util.sh
@@ -0,0 +1,35 @@
+#
+# General test script utilities
+#
+
+if [ -z "$TIMEOUT" ] ; then
+    echo expected TIMEOUT variable defined to its respective command
+    exit 1
+fi
+
+function run_to()
+{
+    maxtime=${1}s
+    shift
+    $TIMEOUT --signal=9 $maxtime "$@"
+}
+
+function mobject_test_start_servers()
+{
+    nservers=${1:-4}
+    startwait=${2:-15}
+    maxtime=${3:-120}
+    cfile=${4:-/tmp/mobject-connect-cluster.gid}
+
+    run_to $maxtime mpirun -np $nservers src/server/mobject-server-daemon na+sm:// $cfile &
+    if [ $? -ne 0 ]; then
+        # TODO: this doesn't actually work; can't check return code of
+        # something executing in background.  We have to rely on the
+        # return codes of the actual client side tests to tell if
+        # everything started properly
+        exit 1
+    fi
+
+    # wait for servers to start
+    sleep ${startwait}
+}
