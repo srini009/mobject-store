b11a914d8a6f5c860c38ee77f60e9dfe3681b2f2
Matthieu Dorier <mdorier@anl.gov>
2017-12-07 13:07:05 +0100

    removed global context

--- include/mobject-server.h
+++ include/mobject-server.h
@@ -13,6 +13,8 @@
 
 #define MOBJECT_SERVER_GROUP_NAME "mobject-store-servers"
 
+typedef struct mobject_server_context mobject_server_context_t;
+
 /**
  * Start a mobject server instance
  *
@@ -20,18 +22,18 @@
  * @param[in] cluster_file  file name to write cluster connect info to
  * @returns 0 on success, negative error code on failure
  */
-int mobject_server_init(margo_instance_id mid, const char *cluster_file);
+mobject_server_context_t* mobject_server_init(margo_instance_id mid, const char *cluster_file);
 
 /**
  * Shutdown a mobject server instance
  * 
  * @param[in] mid   margo instance id
  */
-void mobject_server_shutdown(margo_instance_id mid);
+void mobject_server_shutdown(mobject_server_context_t* svr_ctx);
 
 /**
  * Wait for a mobject server instance to get a shutdown request.
  */
-void mobject_server_wait_for_shutdown(void);
+void mobject_server_wait_for_shutdown(mobject_server_context_t* srv_ctx);
 
 #endif
