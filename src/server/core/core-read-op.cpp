bc5e8250e29cfc4b7c3eb7edc05730af94e3b035
Matthieu Dorier <mdorier@anl.gov>
2017-12-07 11:40:27 +0100

    started integration of bake-bulk

--- src/server/core/core-read-op.cpp
+++ src/server/core/core-read-op.cpp
@@ -1,13 +1,11 @@
 #include <map>
 #include <string>
 #include <iostream>
-#include "src/server/fake/fake-object.hpp"
+#include "src/server/core/core-read-op.h"
 #include "src/server/visitor-args.h"
 #include "src/io-chain/read-op-visitor.h"
 #include "src/io-chain/read-resp-impl.h"
 #include "src/omap-iter/omap-iter-impl.h"
-
-extern std::map< std::string, fake_object > fake_db;
 
 static void read_op_exec_begin(void*);
 static void read_op_exec_stat(void*, uint64_t*, time_t*, int*);
@@ -27,7 +25,7 @@ static struct read_op_visitor read_op_exec = {
 	.visit_end                   = read_op_exec_end
 };
 
-extern "C" void fake_read_op(mobject_store_read_op_t read_op, server_visitor_args_t vargs)
+extern "C" void core_read_op(mobject_store_read_op_t read_op, server_visitor_args_t vargs)
 {
 	execute_read_op_visitor(&read_op_exec, read_op, (void*)vargs);
 }
@@ -40,80 +38,30 @@ void read_op_exec_begin(void* u)
 void read_op_exec_stat(void* u, uint64_t* psize, time_t* pmtime, int* prval)
 {
     auto vargs = static_cast<server_visitor_args_t>(u);
-    std::string name(vargs->object_name);
-    if(fake_db.count(name) == 0) {
-        std::cerr << "[FAKE-BACKEND-WARNING] (stat) Object " << name << " does not exist" << std::endl;
-        *prval = -1;
-        return;
-    }
-    fake_db[name].stat(psize, pmtime);
-	*prval = 0;
 }
 
 void read_op_exec_read(void* u, uint64_t offset, size_t len, buffer_u buf, size_t* bytes_read, int* prval)
 {
     auto vargs = static_cast<server_visitor_args_t>(u);
-    std::string name(vargs->object_name);
-    if(fake_db.count(name) == 0) {
-        std::cerr << "[FAKE-BACKEND-WARNING] (read) Object " << name << " does not exist" << std::endl;
-        *prval = -1;
-        return;
-    }
-    fake_db[name].read(vargs->mid, vargs->client_addr, vargs->bulk_handle, 
-                        buf.as_offset, offset, len, bytes_read);
-	*prval = 0;
 }
 
 void read_op_exec_omap_get_keys(void* u, const char* start_after, uint64_t max_return, 
 				mobject_store_omap_iter_t* iter, int* prval)
 {
     auto vargs = static_cast<server_visitor_args_t>(u);
-    std::string name(vargs->object_name);
-    if(fake_db.count(name) == 0) {
-        std::cerr << "[FAKE-BACKEND-WARNING] (omap_get_keys) Object " << name << " does not exist" << std::endl;
-        *prval = -1;
-        *iter  = NULL;
-        return;
-    }
-
-	omap_iter_create(iter);
-    fake_db[name].omap_get_keys(start_after, max_return, *iter);
-	*prval = 0;
 }
 
 void read_op_exec_omap_get_vals(void* u, const char* start_after, const char* filter_prefix, uint64_t max_return, mobject_store_omap_iter_t* iter, int* prval)
 {
     auto vargs = static_cast<server_visitor_args_t>(u);
-    std::string name(vargs->object_name);
-    if(fake_db.count(name) == 0) {
-        std::cerr << "[FAKE-BACKEND-WARNING] (omap_get_vals) Object " << name << " does not exist" << std::endl;
-        *prval = -1;
-        *iter = NULL;
-        return;
-    }
-
-	omap_iter_create(iter);
-    fake_db[name].omap_get_vals(start_after, filter_prefix, max_return, *iter);
-	*prval = 0;
 }
 
 void read_op_exec_omap_get_vals_by_keys(void* u, char const* const* keys, size_t num_keys, mobject_store_omap_iter_t* iter, int* prval)
 {
     auto vargs = static_cast<server_visitor_args_t>(u);
-    std::string name(vargs->object_name);
-    if(fake_db.count(name) == 0) {
-        std::cerr << "[FAKE-BACKEND-WARNING] (omap_get_vals_by_keys) Object " << name << " does not exist" << std::endl;
-        *prval = -1;
-        *iter = NULL;
-        return;
-    }
-
-	omap_iter_create(iter);
-    std::vector<std::string> keys_vec(keys, keys+num_keys);
-    fake_db[name].omap_get_vals_by_keys(keys_vec, *iter);
-	*prval = 0;
 }
 
 void read_op_exec_end(void* u)
 {
+    auto vargs = static_cast<server_visitor_args_t>(u);
 }
